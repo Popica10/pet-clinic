70e0e00eae36a5182c6de8f6dfd801d5f78e9bca
Popika <andreipop.acs@gmail.com>
2023-01-23 13:49:51 +0200

    Adding POJOs. Closes #2

---
+++ src/main/java/com/example/petclinic/model/Pet.java
@@ -0,0 +1,10 @@
+package com.example.petclinic.model;
+
+import java.time.LocalDateTime;
+
+public class Pet {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDateTime birthday;
+}
