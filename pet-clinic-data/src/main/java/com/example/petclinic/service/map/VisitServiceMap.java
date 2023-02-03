5f019c7af0210fd4a39674108a46469f92c1eb8d
Popika <andreipop.acs@gmail.com>
2023-02-03 17:39:35 +0200

    Implemented Map Impl for Visit Service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/service/map/VisitServiceMap.java
@@ -0,0 +1,18 @@
+package com.example.petclinic.service.map;
+
+import com.example.petclinic.model.Visit;
+import com.example.petclinic.service.VisitService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit,Long> implements VisitService {
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getId() == null ||
+                visit.getPet().getOwner() == null || visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+}
