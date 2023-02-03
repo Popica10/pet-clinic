f19d4f8f7a24995f905f78efe5afdcba18578b08
Popika <andreipop.acs@gmail.com>
2023-02-03 13:23:51 +0200

    Added Spring data Jpa Service for owners. Closes #40

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/service/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,54 @@
+package com.example.petclinic.service.springdatajpa;
+
+import com.example.petclinic.model.Owner;
+import com.example.petclinic.repository.OwnerRepository;
+import com.example.petclinic.service.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+
+    public OwnerSDJpaService(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return ownerRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> ownerSet = new HashSet<>();
+        ownerRepository.findAll().forEach(ownerSet::add);
+        return ownerSet;
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepository.deleteById(aLong);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName).orElse(null);
+    }
+}
+//TODO: How about creating a generic Service for JPA likewise?
