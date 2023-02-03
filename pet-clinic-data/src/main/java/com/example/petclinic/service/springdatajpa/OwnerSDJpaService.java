fe6e7ddcb854a012c4842451ca521e9d1c60105b
Popika <andreipop.acs@gmail.com>
2023-02-03 13:58:42 +0200

    Added Spring data Jpa Service for vets. Closes #41

--- pet-clinic-data/src/main/java/com/example/petclinic/service/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/service/springdatajpa/OwnerSDJpaService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 @Profile("springdatajpa")
---
+++ pet-clinic-data/src/main/java/com/example/petclinic/service/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,47 @@
+package com.example.petclinic.service.springdatajpa;
+
+import com.example.petclinic.model.Vet;
+import com.example.petclinic.repository.VetRepository;
+import com.example.petclinic.service.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return StreamSupport.stream(vetRepository.findAll().spliterator(), false).collect(Collectors.toSet());
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
